3a7440b60ecb2bfa47de271fd3b873a7324712e8
coderxzd <coderxzd@qq.com>
2020-09-02 17:28:13 +0800

    111

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -1,66 +1,31 @@
-// Pages/home/home.js
 Page({
+  data:{
+    name:'CoderWhy',
+    age:18,
+    students:[
+      {id:110,name:'kobe',age:20},
+      {id:111,name:'curry',age:30},
+      {id:112,name:'james',age:40},
+      {id:113,name:'why',age:50},
+    ],
+    counter:0
+  },
+  handleButtonClick(){
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    //1..错误做法，界面不会刷新
+      // console.log(this.data.counter)
+      // this.data.counter+=1
+
+      //2..this.setData()
+      this.setData({
+        counter:this.data.counter +1
+      })
+
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter -1
+    })
   }
 })
